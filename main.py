85daaeefce903d0b45547ae27a4d26fef12cc6ac
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-03-27 10:39:15 +0100

    Musick Tracks were added

--- main.py
+++ main.py
@@ -53,6 +53,60 @@ CREATE TABLE IF NOT EXISTS musictrack (
     album_id INTEGER REFERENCES album(id)
 );
 
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('God\'s Plan', 198, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('In My Feelings', 237, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Nonstop', 192, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Nice for What', 219, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Mob Ties', 193, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('I\'m Upset', 199, 1);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Baby Pluto', 174, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Silly Watch', 173, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Lo Mein', 173, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('That Way', 170, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('P2', 197, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('You Better Move', 183, 2);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Blackstar', 480, 3);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Lazarus', 287, 3);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Sue (Or in a Season of Crime)', 289, 3);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Girl Loves Me', 212, 3);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('Dollar Days', 208, 3);
+
+INSERT INTO musictrack (title, duration, album_id)
+VALUES ('I Can\'t Give Everything Away', 251, 3);
+
 CREATE TABLE IF NOT EXISTS compilation (
     id SERIAL PRIMARY KEY,
     title VARCHAR(20) NOT NULL,

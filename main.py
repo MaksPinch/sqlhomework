41bd8679b8f21801ab9b46a92ab4640f8f4941af
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-03-27 11:03:16 +0100

    compilation_track table was created, values were inserted

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ INSERT INTO genre (title)
 VALUES ('Rock');
 
 INSERT INTO genre (title)
-VALUES ('Jazz')
+VALUES ('Jazz');
+
 
 CREATE TABLE IF NOT EXISTS album (
     id SERIAL PRIMARY KEY,
@@ -45,6 +46,7 @@ VALUES ('Eternal Atake', '2020-03-06');
 
 INSERT INTO album (title, release_date)
 VALUES ('Blackstar', '2016-01-08');
+
 
 CREATE TABLE IF NOT EXISTS musictrack (
     id SERIAL PRIMARY KEY,
@@ -107,6 +109,7 @@ VALUES ('Dollar Days', 208, 3);
 INSERT INTO musictrack (title, duration, album_id)
 VALUES ('I Can\'t Give Everything Away', 251, 3);
 
+
 CREATE TABLE IF NOT EXISTS compilation (
     id SERIAL PRIMARY KEY,
     title VARCHAR(20) NOT NULL,
@@ -128,8 +131,9 @@ VALUES ('David Bowie Greatest Hits', '2019-09-20');
 CREATE TABLE IF NOT EXISTS musician_genre (
     musician_id INTEGER REFERENCES musician(id),
     genre_id INTEGER REFERENCES genre(id),
-    PRIMARY KEY (musicain_id, genre_id)
+    PRIMARY KEY (musician_id, genre_id)
 );
+
 
 INSERT INTO musician_genre (musician_id, genre_id)
 VALUES (1, 1);
@@ -143,20 +147,54 @@ VALUES (3, 3);
 INSERT INTO musician_genre (musician_id, genre_id)
 VALUES (4, 2);
 
+
 CREATE TABLE IF NOT EXISTS album_musician (
-    album_id INTEGER REFERNCES album(id),
-    musicain_id INTEGER REFERNCES musician(id),
+    album_id INTEGER REFERENCES album(id),
+    musician_id INTEGER REFERENCES musician(id),
     PRIMARY KEY (album_id, musician_id)
 );
 
-INSERT INTO album (album_id, musician_id)
-VALUES (1,1);
+-- Связи исполнителей с альбомами
+INSERT INTO album_musician (album_id, musician_id)
+VALUES (1, 1);
 
-INSERT INTO musician_album (musician_id, album_id)
+INSERT INTO album_musician (album_id, musician_id)
 VALUES (2, 2);
 
-INSERT INTO musician_album (musician_id, album_id)
+INSERT INTO album_musician (album_id, musician_id)
 VALUES (3, 3);
 
-INSERT INTO musician_album (musician_id, album_id)
-VALUES (4, 3);
+INSERT INTO album_musician (album_id, musician_id)
+VALUES (3, 4);
+
+
+CREATE TABLE IF NOT EXISTS compilation_track (
+    compilation_id INTEGER REFERENCES compilation(id),
+    musictrack_id INTEGER REFERENCES musictrack(id),
+    PRIMARY KEY (compilation_id, musictrack_id)
+);
+
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (1, 1);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (1, 2);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (2, 7);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (2, 8);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (3, 13);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (3, 14);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (4, 17);
+
+INSERT INTO compilation_track (compilation_id, musictrack_id)
+VALUES (4, 18);
